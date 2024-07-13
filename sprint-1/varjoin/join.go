61534ec191467f8c5ba4d379e39762b0010a709d
Alexander.Taranov <taranov.ad@phystech.edu>
2024-07-13 18:19:53 +0300

    Added Join function.

--- sprint-1/varjoin/join.go
+++ sprint-1/varjoin/join.go
@@ -3,5 +3,14 @@
 package varjoin
 
 func Join(sep string, args ...string) string {
-	return ""
+	if len(args) == 0 {
+		return "" 
+	}
+
+	var res string = args[0]
+	for _, str := range args[1:] {
+		res += sep + str
+	} 
+
+	return res 
 }
