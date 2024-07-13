d5e9becb9cc365b5189a6e205926a8c1d5819dcf
Alexander.Taranov <taranov.ad@phystech.edu>
2024-07-13 20:34:40 +0300

    Added hotels.

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -2,6 +2,10 @@
 
 package hotelbusiness
 
+import (
+	"sort"
+)
+
 type Guest struct {
 	CheckInDate  int
 	CheckOutDate int
@@ -12,6 +16,36 @@ type Load struct {
 	GuestCount int
 }
 
+func create_calendar(guests []Guest) map[int] int {
+	calendar := map[int] int {}
+	for i := range guests {
+		for day := guests[i].CheckInDate; day <= guests[i].CheckOutDate; day++ {
+			if (day == guests[i].CheckOutDate) {
+				calendar[day] = calendar[day]
+			} else {
+				calendar[day]++
+			} 
+		}
+	}
+
+	return calendar
+}
+
+func create_load(calendar map[int] int) []Load {
+	load := []Load{}
+	for day := range calendar {
+		if calendar[day] == calendar[day - 1] {
+			continue
+		}
+		load = append(load, Load{StartDate: day, GuestCount: calendar[day]})
+	}
+
+	sort.SliceStable(load,  func(p, q int) bool {  
+		return load[p].StartDate < load[q].StartDate })
+
+	return load
+}
+
 func ComputeLoad(guests []Guest) []Load {
-	return []Load{}
+	return create_load(create_calendar(guests))
 }
