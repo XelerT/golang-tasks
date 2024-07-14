e1765bdd2219d94583a907971254b3e8b75db82f
Alexander.Taranov <taranov.ad@phystech.edu>
2024-07-14 15:50:00 +0300

    Removed self-assignment.

--- sprint-1/hotelbusiness/hotels.go
+++ sprint-1/hotelbusiness/hotels.go
@@ -16,32 +16,40 @@ type Load struct {
 	GuestCount int
 }
 
-func create_calendar(guests []Guest) map[int] int {
-	calendar := map[int] int {}
+func reserve_table2night(day int, guest_out_date int, calendar *map[int]int) {
+	if day == guest_out_date {
+		_, exists := (*calendar)[day]
+		if !exists {
+			(*calendar)[day] = 0
+		}
+	} else {
+		(*calendar)[day]++
+	}
+}
+
+func create_calendar(guests []Guest) map[int]int {
+	calendar := map[int]int{}
 	for i := range guests {
 		for day := guests[i].CheckInDate; day <= guests[i].CheckOutDate; day++ {
-			if (day == guests[i].CheckOutDate) {
-				calendar[day] = calendar[day]
-			} else {
-				calendar[day]++
-			} 
+			reserve_table2night(day, guests[i].CheckOutDate, &calendar)
 		}
 	}
 
 	return calendar
 }
 
-func create_load(calendar map[int] int) []Load {
+func create_load(calendar map[int]int) []Load {
 	load := []Load{}
 	for day := range calendar {
-		if calendar[day] == calendar[day - 1] {
+		if calendar[day] == calendar[day-1] {
 			continue
 		}
 		load = append(load, Load{StartDate: day, GuestCount: calendar[day]})
 	}
 
-	sort.SliceStable(load,  func(p, q int) bool {  
-		return load[p].StartDate < load[q].StartDate })
+	sort.SliceStable(load, func(p, q int) bool {
+		return load[p].StartDate < load[q].StartDate
+	})
 
 	return load
 }
